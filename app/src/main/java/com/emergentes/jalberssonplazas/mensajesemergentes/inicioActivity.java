7682d1ecb9043c642a2867094d9edacc992cf3de
Jalbersson <jalbersson@gmail.com>
2017-04-12 09:20:30 -0500

    Alertas funcionando

--- app/src/main/java/com/emergentes/jalberssonplazas/mensajesemergentes/inicioActivity.java
+++ app/src/main/java/com/emergentes/jalberssonplazas/mensajesemergentes/inicioActivity.java
@@ -1,5 +1,7 @@
 package com.emergentes.jalberssonplazas.mensajesemergentes;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -35,9 +37,41 @@ public class inicioActivity extends AppCompatActivity implements View.OnClickLis
         switch (v.getId())
         {
             case R.id.btnAlertaOk:
+                AlertDialog.Builder builder= new AlertDialog.Builder(this);
+                builder.setTitle("Alert con OK");
+                builder.setMessage("Alerta, este mensaje sólo tiene OK");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Toast.makeText(getApplicationContext(), "Seleccionó OK", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.show();
                 break;
 
             case R.id.btnAlertaOkCancelar:
+                AlertDialog.Builder builder2= new AlertDialog.Builder(this);
+                builder2.setTitle("Alert con OK y Cancelar");
+                builder2.setMessage("Alerta, este mensaje tiene OK y Cancelar");
+                builder2.setPositiveButton("OK", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Toast.makeText(getApplicationContext(), "Seleccionó OK", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder2.setNegativeButton("Cancelar", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Toast.makeText(getApplicationContext(), "Seleccionó Cancelar", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder2.show();
                 break;
 
             case R.id.btnToastCorto:
